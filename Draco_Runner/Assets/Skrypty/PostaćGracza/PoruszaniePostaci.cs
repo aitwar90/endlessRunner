1e9567b9ed61e9934adcb799ff168bbc9d9b7be9
tomekGol <tomek.golacik@gmail.com>
2021-06-11 13:46:55 +0200

    Poruszanie gracz obsługa input

--- Draco_Runner/Assets/Skrypty/PostaćGracza/PoruszaniePostaci.cs
+++ Draco_Runner/Assets/Skrypty/PostaćGracza/PoruszaniePostaci.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class PoruszaniePostaci : MonoBehaviour
 {
+    public static PoruszaniePostaci poruszaniePostaci = null;
+    private byte tor = 1;
+    public float offsetToru = 1.0f;
     void Awake()
     {
-
+        if(poruszaniePostaci == null)
+        {
+            poruszaniePostaci = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
     }
     
     void Start()
@@ -16,6 +26,29 @@ public class PoruszaniePostaci : MonoBehaviour
 
     void Update()
     {
-        
+        ObsłużInput();
+    }
+    ///<summary>Obsługa strumieni wejścia z klawiszy.</summary>
+    private void ObsłużInput()
+    {
+        if(Input.anyKeyDown)
+        {
+            float x = Input.GetAxisRaw("Horizontal");
+            if(x < 0 && tor > 0)
+            {
+                tor--;
+                this.transform.Translate(-Vector3.right);
+            }
+            else if(tor < 2 && x > 0)
+            {
+                tor++;
+                this.transform.Translate(Vector3.right);
+            }
+        }
+    }
+    ///<summary>Kiedy postać ginie ma zostać wywołana ta metoda.</summary>
+    public void GameOver()
+    {
+        //Wyświetl Canvas po śmierci (Obejrzenie reklamy ma wskrzeszać gracza)
     }
 }
