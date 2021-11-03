89efba52f126a2fe29f8d7f25544d592a3ad2bfe
Oskar Przybysz <flamaster.vin@gmail.com>
2021-11-03 20:08:53 +0100

    Buttony

---
+++ Endless Runner Android/Assets/scripts/MenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public GameObject[] panele;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
